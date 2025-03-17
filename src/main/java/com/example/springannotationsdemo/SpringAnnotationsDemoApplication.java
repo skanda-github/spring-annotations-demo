9fbad8f9a41587b6c7ed5052e6a3be3d62e22eee
Skanda <skanda.td@gmail.com>
2025-03-17 09:08:50 +0530

    Spring Boot Starters

---
+++ src/main/java/com/example/springannotationsdemo/SpringAnnotationsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springannotationsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnnotationsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAnnotationsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springannotationsdemo/SpringAnnotationsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springannotationsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAnnotationsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
